636661a8064d616ebe96a67a4996e7cf16075b30
SashaAkulova <akulova.as@phystech.edu>
2024-06-26 22:22:03 +0300

    Update point_intersection.h

--- Intersections_2/point_intersection.h
+++ Intersections_2/point_intersection.h
@@ -1,6 +1,7 @@
 #ifndef POINT_INTERSECTION_H_INCLUDED
 #define POINT_INTERSECTION_H_INCLUDED
 
+#include "Line.h"
 float Point_intersection (const Line& l1, const Line& l2) {}
 
 #endif // POINT_INTERSECTION_H_INCLUDED
