da266cc9e86947e3be1d14b50a5dd15be7a3cc8b
SashaAkulova <akulova.as@phystech.edu>
2024-06-26 21:22:21 +0300

    Update Determinant.h

--- triangle/Determinant.h
+++ triangle/Determinant.h
@@ -1,7 +1,7 @@
 #ifndef DETERMINANT_FUNCTION_H_INCLUDED
 #define DETERMINANT_FUNCTION_H_INCLUDED
 
-#include "Vector_struct.h"
+#include "Vector.h"
 float determinant(const Vector& a1, const Vector& a2){
     return a1.x * a2.y - a1.y * a2.x;
 }
